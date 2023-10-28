c19474b922c8da34a82dc9142d7b5acc1f5af31c
Christian <christiancettour@gmail.com>
2023-10-28 18:42:16 -0300

    Cambio modulo Crud por Estudiante y mejoras

--- src/App.js
+++ src/App.js
@@ -1,63 +1,67 @@
-import './App.css';
-
+import "./App.css";
 
 //mis compnentes
-import { Header } from './componentes/Header/Header';
-import { Footer } from './componentes/Footer/Footer';
+import { Header } from "./componentes/Header/Header";
+import { Footer } from "./componentes/Footer/Footer";
 
-import { Contacto } from './componentes/Contacto/Contacto';
-import { Crud } from './componentes/Crud/Crud';
-import { Carrera } from './componentes/Carrera/Carrera';
-import { Dashboard } from './componentes/Dashboard/Dashboard';
-import { Inicio } from './componentes/Inicio/Inicio';
-import { Institucional } from './componentes/Institucional/Institucional';
-import { Login } from './componentes/Login/Login';
+import { Contacto } from "./componentes/Contacto/Contacto";
+import { Estudiante } from "./componentes/Estudiante/Estudiante";
+import { Carrera } from "./componentes/Carrera/Carrera";
+import { Dashboard } from "./componentes/Dashboard/Dashboard";
+import { Inicio } from "./componentes/Inicio/Inicio";
+import { Institucional } from "./componentes/Institucional/Institucional";
+import { Login } from "./componentes/Login/Login";
 
-import { UserProvider } from './componentes/UserContext/UserContext';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ProtectedRoute } from './componentes/ProtectedRoute/ProtectedRoute';
-
+import { UserProvider } from "./componentes/UserContext/UserContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "./componentes/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
-    <>
+    <div className="appStyle">
       <BrowserRouter>
         <UserProvider>
           <Header />
           <Routes>
-            <Route path='/' element={<Inicio />} />
-            <Route path='/institucional' element={<Institucional />} />
-            <Route path='/contacto' element={<Contacto />} />
-            <Route path='/crud' element={<Crud />} />
-            <Route path='/carreras' element={<Carrera />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/" element={<Inicio />} />
+            <Route path="/institucional" element={<Institucional />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/estudiante" element={<Estudiante />} />
+            <Route path="/carreras" element={<Carrera />} />
+            <Route path="/login" element={<Login />} />
 
             {/* Ruta protegida para usuarios logueados como DECANO o BEDEL */}
-            <Route path='/privado/dashboard'
+            <Route
+              path="/privado/dashboard"
               element={
                 <ProtectedRoute mustBeBedel={false}>
                   {<Dashboard />}
                 </ProtectedRoute>
-              } />
+              }
+            />
 
             {/* Ruta protegida para usuarios logueados como BEDEL */}
-            <Route path='/privado/crud'
+            <Route
+              path="/privado/estudiante"
               element={
                 <ProtectedRoute mustBeBedel={true}>
-                  {<Crud />}
+                  {<Estudiante />}
                 </ProtectedRoute>
-              } />
-            <Route path='/privado/carreras'
+              }
+            />
+            <Route
+              path="/privado/carreras"
               element={
                 <ProtectedRoute mustBeBedel={true}>
                   {<Carrera />}
                 </ProtectedRoute>
-              } />
+              }
+            />
           </Routes>
         </UserProvider>
         <Footer />
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/componentes/Carrera/Carrera.jsx
+++ src/componentes/Carrera/Carrera.jsx
@@ -1,152 +1,224 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import { Button, Table, Form, Modal } from 'react-bootstrap';
+import { Button, Table, Form, Modal } from "react-bootstrap";
 
-import './carrera.css';
-
+import "./carrera.css";
 
 export function Carrera() {
-    const baseURL = 'http://localhost:3005/api/v1';
+  const baseURL = "http://localhost:3005/api/v1";
 
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    //Manejo del modal
-    const verModal = () => {
-        setShowModal(true);
-    };
-    const cerrarModal = () => setShowModal(false);
+  //Manejo del modal
+  const verModal = () => setShowModal(true);
+  const cerrarModal = () => setShowModal(false);
 
-    // objeto para almacenar la información del formulario
-    const [carrera, setCarrera] = useState({ nombre: '', modalidad: '' });
+  // objeto para almacenar la información del formulario
+  const [carrera, setCarrera] = useState({ nombre: "", modalidad: "" });
 
-    // datos de estudiantes
-    const [datos, setDatos] = useState(null);
+  // datos de carrera
+  const [datos, setDatos] = useState(null);
 
-    useEffect(() => {
+  const [inscriptos, setInscriptos] = useState(null);
+
+  useEffect(() => {
+    buscarCarreras();
+  }, []);
+
+  const buscarCarreras = async () => {
+    axios
+      .get(baseURL + "/carrera/carreras")
+      .then((res) => {
+        console.log(res);
+        setDatos(res.data.dato);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const eliminarCarrera = async (idCarrera) => {
+    axios
+      .delete(baseURL + "/carrera/carreras/" + idCarrera)
+      .then((res) => {
         buscarCarreras();
-    }, []);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const buscarCarreras = async () => {
-        axios.get(baseURL + '/carrera/carreras')
-            .then(res => {
-                console.log(res);
-                setDatos(res.data.dato);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+  const crearCarrera = async (e) => {
+    e.preventDefault();
 
-    const eliminarCarrera = async (idCarrera) => {
-        axios.delete(baseURL + '/carrera/carreras/' + idCarrera)
-            .then(res => {
-                buscarCarreras();
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
+    axios
+      .post(baseURL + "/carrera/carreras", carrera)
+      .then((res) => {
+        if (res.data.estado === "OK") {
+          alert(res.data.msj);
 
-    const crearCarrera = async (e) => {
-        e.preventDefault();
+          cerrarModal();
+          buscarCarreras();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        axios.post(baseURL + '/carrera/carreras', carrera)
-            .then(res => {
-                if (res.data.estado === 'OK') {
-                    alert(res.data.msj);
-                    
-                    cerrarModal();
-                    buscarCarreras();
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            })
+  const mostrarInscriptos = async (idCarrera) => {
+    axios
+      .get(baseURL + "/carrera/inscriptos/" + idCarrera)
+      .then((res) => {
+        setInscriptos(res.data.dato);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    }
+  return (
+    <>
+      <div className="titulo">
+        <h2>CARRERAS</h2>
+      </div>
 
-    return (
-        <>
-            <div className="titulo">
-                <h2>CARRERAS</h2>
-            </div>
+      <div className="container mt-3">
+        
+        <div className="btnDiv">
+          <Button variant="light" onClick={verModal}>
+            Crear carrera
+          </Button>
+        </div>
 
-            <div className="container">
+        <div className="item">
+          <h2>Lista de Carreras</h2>
+        </div>
 
-                <div className="item">
-                    <h2>Lista de Carreras</h2>
+        <div className="container mt-3 mb-5 miTabla">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th className="tabla-thead">Nro. Carrera</th>
+                <th className="tabla-thead">Nombre</th>
+                <th className="tabla-thead">Modalidad</th>
+                <th className="tabla-thead">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {datos ? (
+                datos.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.idCarrera}</td>
+                    <td>{item.nombre}</td>
+                    <td>{item.modalidad}</td>
+                    <td>
+                      <Button
+                        variant="light"
+                        onClick={() => mostrarInscriptos(item.idCarrera)}
+                      >
+                        Ver inscriptos
+                      </Button>
+                      <Button variant="success" className="miBoton">
+                        Editar
+                      </Button>
+                      <Button
+                        variant="danger"
+                        onClick={() => eliminarCarrera(item.idCarrera)}
+                      >
+                        Eliminar
+                      </Button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={4}>No hay carreras para mostrar</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </div>
+
+        <div className="item">
+          <h2>Alumnos inscriptos</h2>
+        </div>
+
+        <div className="container mt-3 mb-5 miTabla">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th className="tabla-thead">Legajo</th>
+                <th className="tabla-thead">DNI</th>
+                <th className="tabla-thead">Nombre</th>
+                <th className="tabla-thead">Apellido</th>
+                <th className="tabla-thead">Correo Electrónico</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inscriptos ? (
+                inscriptos.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.idEstudiante}</td>
+                    <td>{item.dni}</td>
+                    <td>{item.nombre}</td>
+                    <td>{item.apellido}</td>
+                    <td>{item.correoElectronico}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={5}></td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </div>
+        <Modal show={showModal} onHide={cerrarModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Crear carrera</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={(e) => crearCarrera(e)}>
+              <div className="row">
+                <div className="col-md-4">
+                  <Form.Group className="mb-3" controlId="formBasicNombre">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={(e) =>
+                        setCarrera({ ...carrera, nombre: e.target.value })
+                      }
+                      value={carrera.nombre}
+                      required
+                    />
+                  </Form.Group>
                 </div>
 
-                <div className='container mt-1 mb-5 miTabla'>
-                    <Table striped bordered hover >
-                        <thead >
-                            <tr>
-                                <th className='tabla-thead'>Nro. Carrera</th>
-                                <th className='tabla-thead'>Nombre</th>
-                                <th className='tabla-thead'>Modalidad</th>
-                                <th className='tabla-thead'>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                datos ? (datos.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{item.idCarrera}</td>
-                                        <td>{item.nombre}</td>
-                                        <td>{item.modalidad}</td>
-                                        <td>
-                                            <Button variant="success" className='miBoton'>Editar</Button>
-                                            <Button variant="danger" onClick={() => eliminarCarrera(item.idCarrera)}>Eliminar</Button>
-                                        </td>
-                                    </tr>
-                                )))
-                                    :
-                                    (
-                                        <tr>
-                                            <td colSpan={4}>No hay carreras para mostrar</td>
-                                        </tr>
-                                    )
-                            }
-                        </tbody>
-                    </Table>
+                <div className="col-md-6">
+                  <Form.Group className="mb-3" controlId="formBasicModalidad">
+                    <Form.Label>Modalidad</Form.Label>
+                    <Form.Select
+                      onChange={(e) =>
+                        setCarrera({ ...carrera, modalidad: e.target.value })
+                      }
+                    >
+                      <option value="">Seleccionar...</option>
+                      <option value="0">Presencial</option>
+                      <option value="1">Virtual</option>
+                    </Form.Select>
+                  </Form.Group>
                 </div>
-                <Button variant="light" onClick={verModal}>Crear carrera</Button>
-                
-
-                <Modal show={showModal} onHide={cerrarModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Crear carrera</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form onSubmit={e => crearCarrera(e)}>
-                            <div className='row'>
-                                <div className="col-md-4">
-                                    <Form.Group className="mb-3" controlId="formBasicNombre">
-                                        <Form.Label>Nombre</Form.Label>
-                                        <Form.Control type="text"
-                                            onChange={(e) => setCarrera({ ...carrera, nombre: e.target.value })}
-                                            value={carrera.nombre} required />
-                                    </Form.Group>
-                                </div>
-
-                                <div className="col-md-6">
-                                    <Form.Group className="mb-3" controlId="formBasicModalidad">
-                                        <Form.Label>Modalidad</Form.Label>
-                                        <Form.Select onChange={(e) => setCarrera({ ...carrera, modalidad: e.target.value })}>
-                                            <option value="">Seleccionar...</option>
-                                            <option value="0">Presencial</option>
-                                            <option value="1">Virtual</option>
-                                        </Form.Select>
-                                    </Form.Group>
-                                </div>
-                            </div>
-                            <Button variant="light" type="submit">Crear carrera</Button>
-                        </Form>
-                    </Modal.Body>
-                </Modal>
-            </div>
-        </>
-    );
+              </div>
+              <Button variant="light" type="submit">
+                Crear carrera
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
+    </>
+  );
 }
